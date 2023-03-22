121180ca302bd3b5bc8bd2d149467455e39dfef5
Towfiqul Alom <towfiqul.alom@nordcloud.com>
2023-03-22 11:31:20 +0200

    project setup done

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'semantic-ui-css/semantic.min.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "semantic-ui-css/semantic.min.css";
 import { SetGlobalStyle } from "@nordcloud/gnui";
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <SetGlobalStyle customScrollbars />
+    <SetGlobalStyle />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);

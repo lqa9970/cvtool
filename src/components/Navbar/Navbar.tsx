954d3aff70d371ed55a482926b6e25d4b72db42b
Quang Anh Le <quang-anh.le@nordcloud.com>
2023-04-04 09:44:27 +0300

    Navbar initial

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,25 @@
 import { useState } from "react"
-
-import "./navbar.css"
+import "./Navbar.css"
+import { StickyBar } from "@nordcloud/gnui"
 
 const Navbar = () => {
   // const [item, setItem] = useState({})
-  return <h1>Navbar</h1>
+  return (
+    <>
+      <StickyBar>
+        <div className="NavContent">
+          <div className="NavContent left">
+            <a href="#">dashboard</a>
+            <a href="#">projects</a>
+            <a href="#">talents</a>
+          </div>
+          <div className="NavContent right">
+            <a href="profile">profile</a>
+          </div>
+        </div>
+      </StickyBar>
+    </>
+  )
 }
 
 export default Navbar

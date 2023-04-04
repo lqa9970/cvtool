a5faf705951d4ffbc41baca3ddbaa622749aef99
Quang Anh Le <quang-anh.le@nordcloud.com>
2023-04-04 16:53:05 +0300

    adding Sass

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,23 @@
 import { useState } from "react"
-import "./Navbar.css"
-import { StickyBar } from "@nordcloud/gnui"
+import "./Navbar.scss"
+// import { StickyBar } from "@nordcloud/gnui"
 
 const Navbar = () => {
   // const [item, setItem] = useState({})
   return (
     <>
-      <StickyBar>
-        <div className="NavContent">
-          <div className="NavContent left">
-            <a href="#">dashboard</a>
-            <a href="#">projects</a>
-            <a href="#">talents</a>
-          </div>
-          <div className="NavContent right">
-            <a href="profile">profile</a>
-          </div>
+      {/* <StickyBar> */}
+      <div className="NavContent">
+        <div className="NavContent left">
+          <a href="#">dashboard</a>
+          <a href="#">projects</a>
+          <a href="#">talents</a>
         </div>
-      </StickyBar>
+        <div className="NavContent right">
+          <a href="profile">profile</a>
+        </div>
+      </div>
+      {/* </StickyBar> */}
     </>
   )
 }

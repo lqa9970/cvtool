81329ca2532f641f3d3c47ba4cfd74c65eadce62
Md Towfiqul Alom <106370900+towfiqulalom@users.noreply.github.com>
2023-04-11 23:38:21 +0300

    Revert "CVHUB-30 Adding Navbar component"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import Navbar from "./components/Navbar/Navbar"
+import Navbar from "./components/Navbar/Navbar";
+import { Container } from "semantic-ui-react";
+import Dashboard from "./pages/Dashboard";
 
-import "./styles/style.scss"
+import "./styles/style.scss";
 
-function App() {
+const App = () => {
   return (
     <div className="App" id="App">
-      <Navbar />
+        <Navbar />
+        <Dashboard />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import React from "react"
-import "semantic-ui-css/semantic.min.css"
-import ReactDOM from "react-dom/client"
-import "./index.css"
-import App from "./App"
-import reportWebVitals from "./reportWebVitals"
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />

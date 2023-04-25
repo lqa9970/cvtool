63efdeed419f3a861f13bc0e46f7cba18780705b
Utsab Kharel <utsab.kharel@nordcloud.com>
2023-04-25 09:26:03 +0300

    CVHUB-76 Add search component

---
+++ src/components/search/Search.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Search, SearchProps } from 'semantic-ui-react';
+
+type SearchComponentProps = {
+    placeholder: string
+    handleSearchChange: (event: React.MouseEvent<HTMLElement>, data: SearchProps) => void;
+}
+
+const SearchComponent = ({placeholder, handleSearchChange }:SearchComponentProps) => {
+ return (
+    <Search 
+        placeholder={placeholder}
+        onSearchChange={handleSearchChange}
+    />
+ )
+}
+
+export default SearchComponent;

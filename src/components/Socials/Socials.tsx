4eb4e9ec27db51812c8432290ed6fc9f3ab38656
Utsab Kharel <utsab.kharel@nordcloud.com@Utsabs-MacBook-Pro.local>
2023-08-14 12:52:18 +0300

    CVHUB-175: Add validation to social links

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -1,50 +1,82 @@
-import { FormEvent, useState } from "react";
+import { Formik, Form } from "formik";
 import { Button, Grid } from "semantic-ui-react";
 import useUpdateUser from "../../hooks/useUpdateUser";
 import { EmployeeUser } from "../../types/types";
 import SocialLink from "../SocialLink/SocialLink";
 import "./Socials.scss";
+import { linksSchema, SocialLinks } from "./SocialsUtils";
 
 type ISocials = {
   userDetails: EmployeeUser | null;
 };
 
-function Socials(props: ISocials) {
-  const [github, setGitHub] = useState(
-    props.userDetails?.social_links?.github || ""
-  );
-  const [linkedin, setLinkedin] = useState(
-    props.userDetails?.social_links?.linkedin || ""
-  );
-  const [website, setWebsite] = useState(
-    props.userDetails?.social_links?.website || ""
-  );
+function Socials({ userDetails }: ISocials) {
+  const githubUrl = userDetails?.social_links?.github ?? "";
+  const linkedinUrl = userDetails?.social_links?.linkedin ?? "";
+  const websiteUrl = userDetails?.social_links?.website ?? "";
 
   const [updateUser] = useUpdateUser();
 
-  const handleSocialLinks = async (event: FormEvent) => {
-    event.preventDefault();
-    props.userDetails &&
+  const handleFormikSubmit = async ({
+    github,
+    linkedin,
+    website,
+  }: SocialLinks) => {
+    userDetails &&
       (await updateUser(
-        { social_links: { github, linkedin, website } },
-        props.userDetails.id || ""
+        {
+          social_links: { github, linkedin, website },
+        },
+        userDetails.id || ""
       ));
   };
 
   return (
     <Grid.Column>
-      <Grid>
-        <SocialLink link={linkedin} iconName="linkedin" setLink={setLinkedin} />
-        <SocialLink link={github} iconName="github" setLink={setGitHub} />
-        <SocialLink link={website} iconName="world" setLink={setWebsite} />
-        <Grid.Row id="soc-button-row">
-          <Grid.Column>
-            <Button id="soc-add-button" onClick={handleSocialLinks}>
-              Save
-            </Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <Formik
+        validationSchema={linksSchema}
+        initialValues={{
+          github: githubUrl,
+          linkedin: linkedinUrl,
+          website: websiteUrl,
+        }}
+        onSubmit={(values) => handleFormikSubmit(values)}
+      >
+        {({ values, handleChange, handleSubmit, errors, isValid }) => (
+          <Form onSubmit={handleSubmit}>
+            <Grid>
+              <SocialLink
+                fieldName="linkedin"
+                iconName="linkedin"
+                urlValue={values.linkedin}
+                errorMsg={errors.linkedin}
+                onChange={handleChange}
+              />
+              <SocialLink
+                fieldName="github"
+                iconName="github"
+                urlValue={values.github}
+                errorMsg={errors.github}
+                onChange={handleChange}
+              />
+              <SocialLink
+                fieldName="website"
+                iconName="world"
+                urlValue={values.website}
+                errorMsg={errors.website}
+                onChange={handleChange}
+              />
+              <Grid.Row id="soc-button-row">
+                <Grid.Column>
+                  <Button id="soc-add-button" type="submit" disabled={!isValid}>
+                    Save
+                  </Button>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Form>
+        )}
+      </Formik>
     </Grid.Column>
   );
 }

11a4b85e1f87090fca890aae9a28e31e57621982
Utsab Kharel <utsab.kharel@nordcloud.com@Utsabs-MacBook-Pro.local>
2023-08-17 13:53:40 +0300

    Fix Duplicate Skills Selection & Reset options for Skill dropdown after submit

--- src/components/ProjectHistory/ProjectSkills/ProjectSkills.tsx
+++ src/components/ProjectHistory/ProjectSkills/ProjectSkills.tsx
@@ -11,66 +11,68 @@ import {
 
 import "./ProjectSkills.scss";
 import useGetFirestoreCollection from "../../../hooks/useGetCollectionData";
-import { Skills } from "../../../types/types";
+import { Skill } from "../../../types/types";
 import { uniqueIdGenerator } from "../../../utils/uid";
 
 type ProjectSkillsProps = {
   addSkill: (
     field: string,
-    value: Skills[],
+    value: Skill[],
     shouldValidate?: boolean | undefined
   ) => void;
   resetSkills: boolean;
 };
 
-function identify(arr: string[]): Skills[] {
+function identify(arr: string[]): Skill[] {
   return arr.map((item) => {
     return { name: item, id: uniqueIdGenerator() };
   });
 }
 
 function ProjectSkills(props: ProjectSkillsProps) {
-  const [skillSelected, setSkillSelected] = useState<string>("");
-  const [skillsSelected, setSkillsSelected] = useState<string[]>([]);
+  const [isSkillSelected, setIsSkillSelected] = useState<boolean>(false);
+  const [selectedSkill, setSelectedSkill] = useState<string | null>(null);
+  const [selectedSkills, setSelectedSkills] = useState<string[]>([]);
   const [options, setOptions] = useState<DropdownItemProps[]>([]);
 
   const skills = useGetFirestoreCollection({ collection: "skills" });
 
   useEffect(() => {
     if (skills.data) {
-      const skillsOptions = (skills.data as Skills[]).map((skill) => ({
+      const skillsOptions = (skills.data as Skill[]).map((skill) => ({
         key: skill.id,
         value: skill.name,
         text: skill.name,
       }));
       setOptions(skillsOptions);
     }
-  }, [skills.data]);
+  }, [skills.data, props.resetSkills]);
 
   useEffect(() => {
-    setSkillsSelected([]);
+    setSelectedSkills([]);
   }, [props.resetSkills]);
 
-  const onChange = (
-    event: SyntheticEvent<HTMLElement>,
-    data: DropdownProps
-  ) => {
-    setSkillSelected(data.value as string);
+  const onChange = (_: React.SyntheticEvent<HTMLElement>, { value }: DropdownProps) => {
+    if(value){
+      setIsSkillSelected(true)
+      setSelectedSkill(value as string);
+    }
   };
-  const onClick = () => {
-    setSkillsSelected((prev) => {
-      const skillsStrings = [...prev, skillSelected];
-      props.addSkill("skills", identify(skillsStrings));
-      return [skillSelected, ...prev] as string[];
-    });
-    setOptions((prev) => prev.filter((_) => _.text !== skillSelected));
+
+  const handleAddSkill = () => {
+    setSelectedSkill(null); // Clear selected skill after adding
+    setIsSkillSelected(false);
+    const allSkills = [...selectedSkills, selectedSkill as string]
+    setSelectedSkills(allSkills)
+    props.addSkill("skills", identify(allSkills));
+    setOptions(options.filter((_) => _.text !== selectedSkill));
   };
 
   const deleteHandler = (item: string, index: number) => {
-    const filteredSkillsSelected = skillsSelected.filter(
+    const filteredSkillsSelected = selectedSkills.filter(
       (_, index_) => index_ !== index
     );
-    setSkillsSelected(filteredSkillsSelected);
+    setSelectedSkills(filteredSkillsSelected);
     setOptions((prev) => [...prev, { key: item, value: item, text: item }]);
     props.addSkill("skills", identify(filteredSkillsSelected));
   };
@@ -93,10 +95,10 @@ function ProjectSkills(props: ProjectSkillsProps) {
             />
             <div className="p-1" />
             <Button
-              disabled={skillSelected === ""}
+              disabled={!isSkillSelected}
               id="edu-add-button"
               type="button"
-              onClick={onClick}
+              onClick={handleAddSkill}
             >
               Add
             </Button>
@@ -106,7 +108,7 @@ function ProjectSkills(props: ProjectSkillsProps) {
       <Grid.Row>
         <Grid.Column>
           <div className="skill-box">
-            {skillsSelected.map((item, index) => (
+            {selectedSkills.map((item, index) => (
               <Label key={item + index.toString()} className="skill">
                 {item}
                 <Icon

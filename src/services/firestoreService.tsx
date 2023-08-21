66153e0d24816c256e6b8100f335b05bcbbffae6
safiul <safiul.alam@nordcloud.fi>
2023-08-21 10:56:36 +0300

    Initial setup for badges

--- src/services/firestoreService.tsx
+++ src/services/firestoreService.tsx
@@ -13,5 +13,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
